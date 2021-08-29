f91109d009e1252ce5f590ed392f4a6a17286b04
Gakutensoku <76168450+Gakutensoku@users.noreply.github.com>
2021-08-29 15:57:28 +0900

    Revert "Revert "Create ConsoleGetKey.cpp""
    This reverts commit f97f7de836f1dcd065c5da4ccbe8e38e0651c5a0.

---
+++ ConsoleGetKey.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Aキーを押してください" << endl;
+    while (true)
+    {
+        if (GetAsyncKeyState('A'))
+        {
+            cout << "Aキーが押されました" << endl;
+        }
+        Sleep(100);
+    }
+    return 0;
+}
